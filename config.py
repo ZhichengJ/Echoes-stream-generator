3e438dea7c4e14261f3675151390f66190c85114
ZhichengJ <zhichengjiang@outlook.es>
2021-06-29 10:29:51 +0200

    Añadido config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+[ICECAST]
+host = localhost
+port = 8000
+source_pass = password
+
+[MQTT]
+host = vps190.cesvima.upm.es
+port = 1883
+
+[LIQUIDSOAP]
+path = /home/zhicheng/.opam/system/bin/liquidsoap
